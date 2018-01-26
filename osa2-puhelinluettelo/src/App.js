b478a139351a2520ad0872f902fb161270e5911d
ptr5000 <35306800+ptr5000@users.noreply.github.com>
2018-01-26 15:47:01 +0200

    2.9

--- osa2-puhelinluettelo/src/App.js
+++ osa2-puhelinluettelo/src/App.js
@@ -5,39 +5,69 @@ class App extends React.Component {
 		super(props)
 		this.state = {
 			persons: [
-				{ name: 'Arto Hellas' }
+				{ name: 'Arto Hellas', number: '123' }
 			],
-			newName: ''
+			newName: '',
+			newNumber: '',
+			searchField: ''
 		}
 	}
 
 	lisaaNimi = (event) => {
 		event.preventDefault()
+
 		let name = this.state.newName
+		let number = this.state.newNumber
 		let persons = [...this.state.persons]
-		persons.push({ name })
-		this.setState({ newName: '', persons })
+
+		if (persons.filter(p => p.name === name).length === 0) {
+			persons.push({ name, number })
+			this.setState({ newName: '', newNumber: '', persons })
+		}
 	}
 
 	handleNameChange = (event) => {
 		this.setState({ newName: event.target.value })
 	}
-	
+
+	handleNumberChange = (event) => {
+		this.setState({ newNumber: event.target.value })
+	}
+
+	handleSearchChange = (event) => {
+		this.setState({ searchField: event.target.value })
+	}
+
 	render() {
 		return (
 			<div>
 				<h2>Puhelinluettelo</h2>
+
+				<div>
+				<p>rajaa näytettäviä: <input value={this.state.searchField}
+							onChange={this.handleSearchChange} /></p>
+				</div>
+
+				<h1>Lisää uusi</h1>
 				<form onSubmit={this.lisaaNimi}>
 					<div>
-						nimi: <input value={this.state.newName} 
-									onChange={this.handleNameChange}/>
+						nimi: <input value={this.state.newName}
+							onChange={this.handleNameChange} />
+
+						numero: <input value={this.state.newNumber}
+							onChange={this.handleNumberChange} />
 					</div>
 					<div>
 						<button type="submit">lisää</button>
 					</div>
 				</form>
 				<h2>Numerot</h2>
-				{this.state.persons.map(person => <div key={person.name}>{person.name}</div>)}
+				<table>
+					<tbody>
+						{this.state.persons.filter(p => p.name.startsWith(this.state.searchField))
+							.map(person => <tr key={person.name}><td>{person.name}</td><td>{person.number}</td></tr>)}
+					</tbody>
+				</table>
 			</div>
 		)
 	}
