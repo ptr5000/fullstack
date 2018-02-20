c17b4e405a0b3065f376621fe16e13ff34bc542a
ptr5000 <35306800+ptr5000@users.noreply.github.com>
2018-02-20 16:16:46 +0200

    4.12

---
+++ osa4/tests/blog.test.js
@@ -0,0 +1,42 @@
+const supertest = require('supertest')
+const {app} = require('../index')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+test('blogs are returned', async() => {
+  const blogsInDb = await Blog.find({})
+
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body.length).toBe(blogsInDb.length)
+})
+
+test('blog post is created', async() => {
+  await api
+    .post('/api/blogs')
+    .send({title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7})
+    .expect(201)
+})
+
+test('blog ', async() => {
+  let resp = await api
+    .post('/api/blogs')
+    .send({title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/"})
+    .expect(201)
+
+  expect(resp.body['likes']).toBe(0)
+})
+
+test('blog post fails ', async() => {
+  const resp = await api
+    .post('/api/blogs')
+    .send({author: "Michael Chan"})
+    .expect(400)
+})
+
+afterAll(() => {
+  server.close()
+})
