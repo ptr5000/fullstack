dde4a4378b4e66052d4ea0f9f1ffe1ab039cc0c1
ptr5000 <35306800+ptr5000@users.noreply.github.com>
2018-02-23 13:46:19 +0200

    missing files for 4.15

---
+++ osa4/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.model('User', {
+    username: String,
+    name: String,
+    passwordHash: String,
+    adult: Boolean,
+    blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Blog' }]
+  })
+  
+  
+  module.exports = User
+  
