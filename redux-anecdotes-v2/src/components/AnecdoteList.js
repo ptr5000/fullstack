cf53c0a71ba4c44f8f93ec67a96258c10ef7ee3e
ptr5000 <35306800+ptr5000@users.noreply.github.com>
2018-03-05 18:11:45 +0200

    4.7-4.10

--- redux-anecdotes-v2/src/components/AnecdoteList.js
+++ redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -3,30 +3,18 @@ import {vote} from '../reducers/anecdoteReducer.js'
 import {setNotification} from '../reducers/notificationReducer.js'
 import Filter from './Filter'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
 
 class AnecdoteList extends React.Component {
-  componentDidMount() { 
-    const {store} = this.context
-
-    this.unsubscribe = store.subscribe(() => {
-      this.forceUpdate()
-    })
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-
   render() {
     const {store} = this.context
 
-    const anecdotes = store.getState().anecdotes
     return (
       <div>
         <h2>Anecdotes</h2>
-        <Filter store={store} />
-        {anecdotes.sort((a, b) => b.votes - a.votes)
-          .filter(anecdote => anecdote.content.indexOf(store.getState().filter.filter) > -1)
+        <Filter />
+        {this.props.anecdotes.sort((a, b) => b.votes - a.votes)
           .map(anecdote =>
           <div key={anecdote.id}>
             <div>
@@ -49,7 +37,19 @@ class AnecdoteList extends React.Component {
   }
 }
 
+
 AnecdoteList.contextTypes = {
   store: PropTypes.object
 }
-export default AnecdoteList
+
+
+const mapStateToProps = (state) => {
+  return {
+    anecdotes: state.anecdotes.filter(anecdote => anecdote.content.indexOf(state.filter.filter) > -1)
+  }
+}
+
+const AnecdoteListConnected = connect(mapStateToProps)(AnecdoteList)
+
+
+export default AnecdoteListConnected
